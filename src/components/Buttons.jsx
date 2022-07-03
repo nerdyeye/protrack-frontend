7455029f1f65561cc3f78a86171b18871295c437
mfonthom <thompsonmfonabasi@gmail.com>
2022-07-03 16:53:01 +0100

    updated button component

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { pri500, sec500, success500, warn500, err500, info500, grey500 } from './Colors';
+import { pri500, pri700, sec500, success500, warn500, err500, info500, grey500, success700, sec700, warn700, err700, info700, grey700 } from './Colors';
 
 export const Button = styled.button`
     font-size: ${props=>props.size==='large'? '24px' :
@@ -38,6 +38,38 @@ export const Button = styled.button`
     props.color === 'info'? info500:
     props.color === 'grey'? grey500:
     pri500};
+    
+    &:hover{
+        background: ${props => props.type === 'outline' || props.type === 'text' ? 'none':
+            props.color ==='secondary' ? sec700 :
+            props.color ==='success' ? success700:
+            props.color ==='warning' ? warn700:
+            props.color ==='error' ? err700:
+            props.color ==='info'? info700:
+            props.color ==='grey'? grey700:
+                pri700};
+        cursor: pointer;
+        border: ${props=>props.type==='outline'? `2px solid 
+            ${props.color === 'secondary'? sec700 : 
+            props.color === 'success'? success700 :
+            props.color === 'warning'? warn700: 
+            props.color === 'error'? err700:
+            props.color === 'info'? info700:
+            props.color === 'grey'? grey700:
+            pri700}`
+            :'none'
+        };    
+        color: ${props=>
+            props.type !=='outline' && props.type !== 'text'? '#fff':
+            props.color === 'secondary' ? sec700 :
+            props.color === 'success'? success700 :
+            props.color === 'warning'? warn700:
+            props.color === 'error' ? err700:
+            props.color === 'info' ? info700:
+            props.color === 'grey' ? grey700:
+            props.type ==='outline' || props.type ==='text' ? pri700:
+            '#fff'};
+    }
 
     ${
         props => props.sx
